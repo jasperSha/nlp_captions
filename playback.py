6670fad88640ebb047bd56fb67761d3de3334c48
Jasper Sha <jaspersha@terracotta.local>
2021-08-02 16:42:26 -0700

    added playback for m1 using sox

--- playback.py
+++ playback.py
@@ -1,4 +1,6 @@
 import subprocess
+import platform
+import sox
 
 
 def play(sound_file):
@@ -7,12 +9,28 @@ def play(sound_file):
     :param sound_file:
     :return:
     '''
+    current_os = platform.system()
+
+    # alsa on ubuntu -> aplay; sox on mac -> play
+    if current_os == 'Darwin':
+        os_cmd = 'play'
+    elif current_os == 'Linux':
+        os_cmd = 'aplay'
+    else:
+        print("unknown OS, can't play audio in terminal!")
+        return
+
     extension = sound_file.split('.')[-1]
     if extension == 'wav':
-        subprocess.Popen(['aplay', sound_file])
-    elif extension == 'flac':
+        print('running wav file')
+        subprocess.Popen([os_cmd, sound_file])
+    elif extension == 'flac' and current_os == 'Darwin':
+        print('running flac file on mac architecture')
+        subprocess.call([os_cmd, sound_file])
+    elif extension == 'flac' and current_os == 'Linux':
+        print('running flac file on linux architecture')
         flac = subprocess.Popen(('flac', '-c', '-d', sound_file), stdout=subprocess.PIPE)
-        out = subprocess.check_output(('aplay'), stdin=flac.stdout)
+        out = subprocess.check_output((os_cmd), stdin=flac.stdout)
         flac.wait()
     else:
         print('unhandled filetype; extension is: ', extension)
